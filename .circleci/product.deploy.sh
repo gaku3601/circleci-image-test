d56ce8cdca0cf3acc29f18af9d1e3c2032d52874
gaku3601 <pro.gaku@gmail.com>
2018-05-10 04:20:50 +0900

    test up

--- .circleci/product.deploy.sh
+++ .circleci/product.deploy.sh
@@ -1,6 +1,6 @@
 faas-cli version
 
 docker login registry.gitlab.com -u ${UserName} -p ${Password}
-faas-cli build -f hello-faas.yml
-faas-cli push -f hello-faas.yml
+faas-cli build -f app.yml
+faas-cli push -f app.yml
 
